c579f1193557975d46f3d37d562a5db034b9d045
Bobbi York <york1670@gmail.com>
2018-09-28 21:46:53 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,7 +15,7 @@ end
 def assign_rooms(attendees)
   hash = Hash.new
   room_assignments = hash
-  attendees.each_char_with_index  do |attendee,index|
+  attendees.each_with_index  do |attendee,index|
       hash[attendee] = index
       return "Hello, #{attendee}! You'll be assigned to room #{index}!"
     end
