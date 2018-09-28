0100b7480276e381d2c4599f457aed02fc3785af
Bobbi York <york1670@gmail.com>
2018-09-28 22:17:14 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,8 +13,9 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
+  room_assignments = []
   attendees.each.with_index(1) do |attendee,index|
-      room_assignment = "Hello, #{attendees}! You'll be assigned to room #{index}"
+      room_assignment = "Hello, #{attendee}! You'll be assigned to room #{index}"
       room_assignments << room_assignments
     end
     return room_assignments
