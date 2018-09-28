59b500c6a40c3bfae062a85766e6abea8db3dcaa
Bobbi York <york1670@gmail.com>
2018-09-28 21:36:07 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def badge_maker(name)
+  name = "Arel"
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.each do |name|
+    badges << "Hello, my name is #{name}."
+    end
+    return badges.each do |badge|
+    end
+end
+
+def assign_rooms(attendees)
+  hash = Hash.new
+  room_assignments = hash
+  attendees.each.with_index {|attendee,index|
+      hash[attendee] = index return "Hello, #{attendee}! You'll be assigned to room #{index}!"}
+      
+      end
+end
